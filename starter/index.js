cc592fa31658ee4e913babf3da4d4826c7203edb
Kiki <146773290+Naike-B@users.noreply.github.com>
2024-01-20 17:21:46 +0000

    Added the createTeam and addEngineer functions to index.js

--- starter/index.js
+++ starter/index.js
@@ -20,6 +20,7 @@ let team = [];
 and the user is prompted to answer a few questions to gather the employee's data */
 // the answers provided by the user are checked and validated - the user is prompted with the same question until a valid input is provided
 // once all questions are answered, creates a new Manager object with the user answers and pushes it to the team array
+// calls the function createTeam to add team members
 function application() {
     function createManager() {
         inquirer.prompt([
@@ -44,7 +45,7 @@ function application() {
                     }
                     return 'Please enter a number greater than 0';
                 }
-            }, 
+            },
             {
                 type: 'input',
                 message: "What\'s the team Manager\'s email address?",
@@ -72,8 +73,94 @@ function application() {
         ]).then(answers => {
             const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
             team.push(manager);
+            createTeam();
         })
     }
-   createManager();
+    // the function createTeam prompts the user to choose between the option of creating a new engineer or intern or the option to finalise the creation of the team 
+    // then the functions addEngineer, addIntern and buildTeam are run based on the user's selected option
+    function createTeam() {
+        inquirer.prompt([
+            {
+                type: 'list',
+                message: "Would you like to:",
+                choices: ["Add an Engineer?", "Add an Intern?", "Finish building the team?"],
+                name: 'menu'
+            },
+        ]).then(answers => {
+            if (answers.menu === "Add an Engineer?") {
+                addEngineer();
+                return;
+            } else if (answers.menu === "Add an Intern?") {
+                addIntern();
+                return;
+            } else {
+                buildTeam();
+                return;
+            }
+        })
+    }
+    // addEngineer prompts the user to answer a few question to gather the employee's data 
+    // the answers provided by the user are checked and validated - the user is prompted with the same question until a valid input is provided
+    // once all questions are answered, creates a new Engineer object with the user answers and pushes it to the team array
+    // calls again createTeam to prompt user to add a new employee or build the team
+    function addEngineer() {
+        inquirer.prompt([
+            {
+                type: 'input',
+                message: "What\'s the Engineer\'s name?",
+                name: 'name',
+                validate: (value) => {
+                    if (value !== '') {
+                        return true;
+                    }
+                    return 'Please enter a valid name';
+                }
+            },
+            {
+                type: 'input',
+                message: "What\'s the Engineer\'s ID(number)?",
+                name: 'id',
+                validate: (value) => {
+                    if (!isNaN(value) && value > 0) {
+                        return true;
+                    }
+                    return 'Please enter a number greater than 0';
+                }
+            },
+            {
+                type: 'input',
+                message: "What\'s the Engineer\'s email address?",
+                name: 'email',
+                validate: (value) => {
+                    const validMail = value.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/);
+                    if (validMail) {
+                        return true;
+                    }
+                    return 'Please enter a valid email address';
+                }
+            },
+            {
+                type: 'input',
+                message: "What\'s the Engineer\'s GitHub username?",
+                name: 'github',
+                validate: (value) => {
+                    if (value !== '') {
+                        return true;
+                    }
+                    return 'Please enter a valid username';
+                }
+            }
+
+        ]).then(answers => {
+            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+            team.push(engineer);
+            createTeam();
+        })
+    }
+    function addIntern() {
+        team.push(intern);
+    }
+
+    createManager();
 }
 application();
