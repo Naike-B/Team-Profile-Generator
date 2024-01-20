143149dc549a85e66825035a6bc995bc4b4cc483
Kiki <146773290+Naike-B@users.noreply.github.com>
2024-01-20 13:30:50 +0000

    Created the Employee class in Employee.js and exported it

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,30 @@
-// TODO: Write code to define and export the Employee class
+// defines the Employee class 
+// the constructor method initialises an object of the class
+// assign the parameters passed to the contructor to the object properties
+class Employee {
+    contructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    // method to get the name of the employee
+    getName() {
+        return this.name;
+    }
+    // method to get the id of the employee
+    getId() {
+        return this.id;
+    }
+    //method to get the email of the employee
+    getEmail() {
+        return this.email;
+    }
+    // method to get the role of the employee which is the name of the constructor
+    getRole() {
+        return this.contructor.name;
+    }
+}
+
+// exports the Employee class so it can be imported and used in other modules
+module.exports = Employee;
+
